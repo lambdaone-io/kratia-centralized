6abbefa98c9a6e35746283920f612bf27d3cc931
Assen Kolov <assen.kolov@gmail.com>
2018-12-02 17:36:07 +0100

    wip

---
+++ src/main/scala/lambdaone/toolbox/sql/CrudPickSqlCollector.scala
@@ -0,0 +1,36 @@
+package lambdaone.toolbox.sql
+
+import cats._
+import doobie._
+import doobie.implicits._
+import lambdaone.kratia.collector.CollectorCRUD.BoxData
+import lambdaone.toolbox.CRUDPick
+
+object CrudPickSqlRegistry {
+
+  def createTable[F[_] : Monad](implicit xa: Transactor[F]) =
+    sql"create table collector (community varchar(255), member varchar(255))".update.run.transact(xa)
+
+  def dropTable[F[_] : Monad](implicit xa: Transactor[F]) =
+    sql"drop table if exists collector".update.run.transact(xa)
+
+  def apply[F[_] : Monad, A: Read : Put, P, D](implicit xa: Transactor[F]) = new CRUDPick[F, A, BoxData[A, P, D]] {
+    /** Stores `a` with the chosen new unique reference */
+    override def create(a: BoxData[A, P, D], id: A): F[A] = ???
+
+    /** Uses a reference to try to look for the data in the store */
+    override def get(id: A): F[Option[BoxData[A, P, D]]] = ???
+
+    /** Uses a reference to try to look for the data in the store, if found, applies `f` and stores the result,
+      * returns the new version if the data if successful */
+    override def update(id: A)(f: BoxData[A, P, D] => BoxData[A, P, D]): F[Option[BoxData[A, P, D]]] = ???
+
+    /** Uses a reference to try to delete the data, returns it if successful */
+    override def delete(id: A): F[Option[BoxData[A, P, D]]] = ???
+
+    /** Returns all data within the store */
+    override def all: F[Map[A, BoxData[A, P, D]]] = ???
+  }
+
+
+}
