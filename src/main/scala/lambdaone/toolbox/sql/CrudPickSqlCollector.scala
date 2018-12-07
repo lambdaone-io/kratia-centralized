1e66098a86ef6ac26f076b2eca809cb9c9d48df3
Assen Kolov <assen.kolov@gmail.com>
2018-12-07 20:10:45 +0100

    wip

--- src/main/scala/lambdaone/toolbox/sql/CrudPickSqlCollector.scala
+++ src/main/scala/lambdaone/toolbox/sql/CrudPickSqlCollector.scala
@@ -4,53 +4,90 @@ import cats._
 import cats.implicits._
 import doobie._
 import doobie.implicits._
-import lambdaone.kratia.collector.{BinaryProposal, InfluenceAllocation}
 import lambdaone.kratia.collector.CollectorCRUD.{AllVotes, BoxData}
+import lambdaone.kratia.collector.{
+  BinaryProposal,
+  InfluenceAllocation,
+  Timestamp
+}
 import lambdaone.toolbox.CRUDPick
 
 object CrudPickSqlCollector {
 
   type BinaryBoxData[A, D] = BoxData[A, BinaryProposal, D]
 
-
-  def init[F[_] : Monad](implicit xa: Transactor[F]) =
-    ( sql"create table ballotbox_binary (id varchar(255), closed_on number)".update.run
-      *> sql"create table votes_binary (box varchar(255), member varchar(255), yes number, no number )".update.run
-      )
+  def init[F[_]: Monad](implicit xa: Transactor[F]) =
+    (sql"create table ballotbox_binary (id varchar(255), data varchar(255), closes_on number)".update.run
+      *> sql"create table votes_binary (box varchar(255), member varchar(255), proof varchar(255), yes number, no number )".update.run)
       .transact(xa)
 
-  def tearDown[F[_] : Monad](implicit xa: Transactor[F]) =
-    ( sql"drop table if exists ballotbox_binary".update.run
-      *> sql"drop table if exists votes_binary".update.run
-      ).transact(xa)
+  def tearDown[F[_]: Monad](implicit xa: Transactor[F]) =
+    (sql"drop table if exists ballotbox_binary".update.run
+      *> sql"drop table if exists votes_binary".update.run).transact(xa)
 
-  def apply[F[_] : Monad, A: Read : Put, D](implicit xa: Transactor[F], readBox: Read[BinaryBoxData[A, D]], readVotes: Read[AllVotes[A, BinaryProposal]])
-  = new CRUDPick[F, A, BoxData[A, BinaryProposal, D]] {
+  def apply[F[_]: Monad, A: Read: Put, D: Read: Put](
+    implicit xa: Transactor[F]
+  ) = new CRUDPick[F, A, BoxData[A, BinaryProposal, D]] {
+
     /** Stores `a` with the chosen new unique reference */
-    override def create(a: BoxData[A, BinaryProposal, D], id: A): F[A] =
-      sql"insert into ballotbox_binary (id, closes_on) values ($id, ${a.closedOn})".update.run.transact(xa)
-        .map {
-          case 1 => id
-          // FIXME we can't recovers from error here. Change return type to F[Option[]], or use MonadError?
+    override def create(a: BoxData[A, BinaryProposal, D], id: A): F[A] = {
+      sql"insert into ballotbox_binary (id, data, closes_on) values ($id, ${a.data}, ${a.closedOn})".update.run
+        .flatMap {
+          case 1 =>
+            a.votes
+              .map {
+                case (member, (proof, InfluenceAllocation(ia))) =>
+                  val yes: Double = ia.get(BinaryProposal.Yes).getOrElse(0.0)
+                  val no: Double = ia.get(BinaryProposal.No).getOrElse(0.0)
+                  val y: doobie.ConnectionIO[Int] =
+                    sql"insert into votes_binary (box, member, proof, yes, no) values ($id, ${member}, ${proof}, ${yes}, ${no})".update.run
+                  // FIXME we can't recovers from error here. Change return type to F[Option[]], or use MonadError?
+                  y
+              }
+              .toList
+              .sequence
+
         }
+        .transact(xa)
+        .map { _ => id
+        }
+    }
 
     /** Uses a reference to try to look for the data in the store */
     // FIXME: In one transaction
     override def get(id: A): F[Option[BoxData[A, BinaryProposal, D]]] = {
-      val ballotData: F[Option[BoxData[A, BinaryProposal, D]]] = sql"select closed_on, data from ballotbox_binary where id=${id}"
-        .query[BoxData[A, BinaryProposal, D]]
-        .option.transact(xa)
+      val ballotData: F[Option[BoxData[A, BinaryProposal, D]]] =
+        sql"select closes_on, data from ballotbox_binary where id=${id}"
+          .query[(Timestamp, D)]
+          .map {
+            case (ts, d) =>
+              BoxData[A, BinaryProposal, D](BinaryProposal.ballot, ts, d, Map())
+          }
+          .option
+          .transact(xa)
 
       ballotData.flatMap { opt =>
-        val votes: F[List[(A, Double, Double)]] = sql"select member, yes, no from votes_binary where box = $id"
-          .query[(A, Double, Double)].to[List].transact(xa)
+        val votes: F[List[(A, A, Double, Double)]] =
+          sql"select member, proof, yes, no from votes_binary where box = $id"
+            .query[(A, A, Double, Double)]
+            .to[List]
+            .transact(xa)
 
-        // FIXME proof of vote needed here
         val allVotes: F[AllVotes[A, BinaryProposal]] = votes.map { l =>
-          val tuples = l.map { case (a, yes, no) => (a, (a, InfluenceAllocation(Map(BinaryProposal.Yes -> yes, BinaryProposal.No -> no)))) }
+          val tuples = l.map {
+            case (a, proof, yes, no) =>
+              (
+                a,
+                (
+                  proof,
+                  InfluenceAllocation(
+                    Map(BinaryProposal.Yes -> yes, BinaryProposal.No -> no)
+                  )
+                )
+              )
+          }
           Map(tuples: _*)
         }
-
         allVotes.map(v => opt.map(bd => bd.copy(votes = v)))
       }
 
@@ -58,13 +95,39 @@ object CrudPickSqlCollector {
 
     /** Uses a reference to try to look for the data in the store, if found, applies `f` and stores the result,
       * returns the new version if the data if successful */
-    override def update(id: A)(f: BoxData[A, BinaryProposal, D] => BoxData[A, BinaryProposal, D])
-    : F[Option[BoxData[A, BinaryProposal, D]]] = {
+    override def update(id: A)(
+      f: BoxData[A, BinaryProposal, D] => BoxData[A, BinaryProposal, D]
+    ): F[Option[BoxData[A, BinaryProposal, D]]] = {
       val before: F[Option[BoxData[A, BinaryProposal, D]]] = get(id)
-      before.map { optBoxData =>
+      val after: F[Option[BoxData[A, BinaryProposal, D]]] = before.map(_.map(f))
 
+      val xx: F[Option[F[List[Int]]]] = (before, after).mapN { (optB, optA) =>
+        (optB, optA).mapN { (b, a) =>
+          val addedVotes
+            : Set[(A, (A, InfluenceAllocation[BinaryProposal]))] = a.votes.toSet diff b.votes.toSet
+          val removedVotes = b.votes.toSet diff a.votes.toSet
 
+          val added: Set[doobie.ConnectionIO[Int]] = addedVotes.map {
+            case (member, (proof, InfluenceAllocation(ia))) =>
+              val yes: Double = ia.get(BinaryProposal.Yes).getOrElse(0.0)
+              val no: Double = ia.get(BinaryProposal.No).getOrElse(0.0)
+              sql"insert into votes_binary (box, member, proof, yes, no) values ($id, ${member}, ${proof}, ${yes} ${no})".update.run
+          }
+          val deleted: Set[doobie.ConnectionIO[Int]] = removedVotes.map {
+            case (member, _) =>
+              sql"delete from votes_binarywhere box = $id, and member = ${member}".update.run
+          }
+
+          (added ++ deleted).toList.sequence.transact(xa)
+        }
       }
+
+      val ffList: F[F[List[Int]]] = xx.map(_.getOrElse(Monad[F].pure(List())))
+      val fList: F[List[Int]] = ffList.flatMap { i => i
+      }
+      fList.flatMap { _ => after
+      }
+
     }
 
     /** Uses a reference to try to delete the data, returns it if successful */
@@ -73,6 +136,5 @@ object CrudPickSqlCollector {
     /** Returns all data within the store */
     override def all: F[Map[A, BoxData[A, BinaryProposal, D]]] = ???
   }
-
 
 }
--- src/test/scala/lambdaone/toolbox/sql/CrudPickSqlCollectorTest.scala
+++ src/test/scala/lambdaone/toolbox/sql/CrudPickSqlCollectorTest.scala
@@ -3,10 +3,11 @@ package lambdaone.toolbox.sql
 import cats.effect.IO
 import cats.implicits._
 import doobie.Transactor
-import lambdaone.kratia.collector.CollectorCRUD.{AllVotes, BoxData}
-import lambdaone.kratia.collector.{BallotBox, BinaryProposal, InfluenceAllocation}
-import lambdaone.kratia.influence.InfluenceDistribution
+import lambdaone.kratia.collector.BinaryProposal.{No, Yes}
+import lambdaone.kratia.collector.CollectorCRUD.BoxData
+import lambdaone.kratia.collector.{BinaryProposal, InfluenceAllocation}
 import org.scalacheck.Gen
+import org.scalacheck.Gen.choose
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
@@ -19,43 +20,105 @@ object CollectorEntityGenerators {
 
   val addressGen: Gen[Address] = Gen.uuid.map(_.toString)
 
-  val boxDataGen: Gen[BoxData[Address, BinaryProposal, String]] =
+  val emptyBoxDataGen: Gen[BoxData[Address, BinaryProposal, String]] =
     for {
       data <- Gen.alphaStr
       now = System.currentTimeMillis
     } yield BoxData(BinaryProposal.ballot, now + 10000, data, Map())
+
+  val boxDataWithOneVoteGen: Gen[BoxData[Address, BinaryProposal, String]] =
+    for {
+      emptyBox <- emptyBoxDataGen
+      member <- addressGen
+      proofOfVote <- addressGen
+      yesInfluence <- choose(0.0, 1.0)
+    } yield
+      emptyBox.copy(
+        votes = Map(
+          proofOfVote -> (member, InfluenceAllocation(
+            Map(Yes -> yesInfluence, No -> (1.0 - yesInfluence))
+          ))
+        )
+      )
+
+  val boxDataWithTwoVotesGen: Gen[BoxData[Address, BinaryProposal, String]] =
+    for {
+      boxDataWithOneVote <- boxDataWithOneVoteGen
+      member <- addressGen
+      proofOfVote <- addressGen
+      yesInfluence <- choose(0.0, 1.0)
+    } yield
+      boxDataWithOneVote.copy(
+        votes = boxDataWithOneVote.votes ++ Set(
+          proofOfVote -> (member, InfluenceAllocation(
+            Map(Yes -> yesInfluence, No -> (1.0 - yesInfluence))
+          ))
+        )
+      )
 }
 
-class CrudPickSqlCollectorTest extends FlatSpec with PropertyChecks with Matchers with BeforeAndAfter {
+class CrudPickSqlCollectorTest
+    extends FlatSpec
+    with PropertyChecks
+    with Matchers
+    with BeforeAndAfter {
 
   import CollectorEntityGenerators._
 
   implicit val cs = IO.contextShift(ExecutionContext.global)
 
   implicit val xa = Transactor
-    .fromDriverManager[IO]("org.h2.Driver", "jdbc:h2:mem:kratia;DB_CLOSE_DELAY=-1")
+    .fromDriverManager[IO](
+      "org.h2.Driver",
+      "jdbc:h2:mem:kratia;DB_CLOSE_DELAY=-1"
+    )
 
   val store = CrudPickSqlCollector[IO, Address, String]
 
   before {
-    ( CrudPickSqlCollector.tearDown[IO] *> CrudPickSqlCollector.init[IO] ).unsafeRunSync()
+    (CrudPickSqlCollector.tearDown[IO] *> CrudPickSqlCollector.init[IO])
+      .unsafeRunSync()
   }
-
 
   "Storing" should "work" in {
-    forAll(addressGen, boxDataGen) { (a: Address, boxData: BoxData[Address, BinaryProposal, String]) =>
-      store.create(boxData, a).unsafeRunSync() shouldBe a
-    }
-  }
-  "Adding votes" should "work" in {
-    forAll(addressGen, addressGen, addressGen, boxDataGen) { (a: Address, voter: Address, proof: Address, boxData: BoxData[Address, BinaryProposal, String]) =>
-      ( store.create(boxData, a) *>
-        store.update(a)(
-          bd => bd.copy(votes = bd.votes +
-            ((voter, (proof, InfluenceAllocation(Map(BinaryProposal.Yes -> 1.0, BinaryProposal.No -> 0.0)))))
-          )) ).unsafeRunSync() shouldBe a
+    forAll(addressGen, emptyBoxDataGen) {
+      (a: Address, boxData: BoxData[Address, BinaryProposal, String]) =>
+        store.create(boxData, a).unsafeRunSync() shouldBe a
     }
   }
 
+//  "Adding votes" should "work" in {
+//    forAll(addressGen, addressGen, addressGen, boxDataWithOneVoteGen) {
+//      (a: Address,
+//       voter: Address,
+//       proof: Address,
+//       boxData: BoxData[Address, BinaryProposal, String]) =>
+//        (store.create(boxData, a) *>
+//          store.update(a)(
+//            bd => bd.copy(votes = bd.votes + ( (voter, (proof, InfluenceAllocation(Map(BinaryProposal.Yes -> 1.0, BinaryProposal.No -> 0.0))
+//                    )) )))
+//          ).unsafeRunSync() shouldBe a
+//    }
+//  }
+
+  "Stored ballot box without votes" should "be read back OK" in {
+    forAll(addressGen, emptyBoxDataGen) { (a, boxData) =>
+      (store.create(boxData, a) *>
+        store.get(a)).unsafeRunSync() shouldBe Some(boxData)
+    }
+  }
+
+  "Stored ballot box with 1 vote" should "be read back OK" in {
+    forAll(addressGen, boxDataWithOneVoteGen) { (a, boxData) =>
+      (store.create(boxData, a) *>
+        store.get(a)).unsafeRunSync() shouldBe Some(boxData)
+    }
+  }
+  "Stored ballot box with 2 votes" should "be read back OK" in {
+    forAll(addressGen, boxDataWithTwoVotesGen) { (a, boxData) =>
+      (store.create(boxData, a) *>
+        store.get(a)).unsafeRunSync() shouldBe Some(boxData)
+    }
+  }
 
 }
